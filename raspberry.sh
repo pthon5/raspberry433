a7adbce74d8408056c3e40a8667af493bb55ab9a
pthon5 <53282687+pthon5@users.noreply.github.com>
2020-11-25 13:31:06 +0500

    Add files via upload

---
+++ raspberry.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python3-pip
+sudo pip3 install rpi-rf
+mkdir raspberry433
+wget -P raspberry433 https://raw.githubusercontent.com/pthon5/raspberry433/main/rpi-rf_receive
+> first_install_complete
