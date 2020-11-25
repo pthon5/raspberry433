8d1f7fc6988d7b5f4d4f2bbd14ce97d38892fc2a
pthon5 <53282687+pthon5@users.noreply.github.com>
2020-11-25 13:40:22 +0500

    Update raspberry.sh

--- raspberry.sh
+++ raspberry.sh
@@ -1,6 +1,6 @@
 sudo apt-get install python3-pip
 sudo pip3 install rpi-rf
-mkdir ./raspberry433
-wget -P ./raspberry433/ https://raw.githubusercontent.com/pthon5/raspberry433/main/rpi-rf_receive
-cd ./raspberry433
+mkdir ./"raspberry433"
+wget -P ./"raspberry433" https://raw.githubusercontent.com/pthon5/raspberry433/main/rpi-rf_receive
+cd ./"raspberry433"
 > first_install_complete
