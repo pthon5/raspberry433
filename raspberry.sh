9c2c648816ea955d1aab319c2a86c813642bd708
pthon5 <53282687+pthon5@users.noreply.github.com>
2021-02-12 18:45:37 +0500

    Delete raspberry.sh

--- raspberry.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install python3-pip
-sudo pip3 install rpi-rf
-mkdir ./"raspberry433"
-wget -P ./"raspberry433" https://raw.githubusercontent.com/pthon5/raspberry433/main/rpi-rf_receive
-cd ./"raspberry433"
-> first_install_complete
